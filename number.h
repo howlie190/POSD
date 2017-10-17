7643e8f54a532c469581d90b8a0a6f2808e333c9
Justin <ahkgc360@gmail.com>
2017-10-17 16:45:07 +0800

    no message

--- number.h
+++ number.h
@@ -1,12 +1,12 @@
 #ifndef number_h
 #define number_h
 #include "term.h"
-class Number : public Term {
+class number : public Term {
 private:
     const string _symbol;
     const double _value;
 public:
-    Number(double);
+    number(double);
     string symbol() const;
     string value() const;
     bool match(Term &term);
